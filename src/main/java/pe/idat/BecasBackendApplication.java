6f4f39f90d75e3679eb2543b29872259458aceb8
ZYDFry <zydiegofuni2050@gmail.com>
2025-12-10 13:03:41 -0500

    Estructura Base del proyecto Becas

---
+++ src/main/java/pe/idat/BecasBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecasBackendApplication.class, args);
+	}
+
+}
